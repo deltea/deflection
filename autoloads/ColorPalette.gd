a2b19b66abe40c5fcab1d079ae66d1b8ce0c10f9
Leo Feng <thcheetah777@gmail.com>
2024-02-09 19:12:23 -0600

    ❗ setup

---
+++ autoloads/ColorPalette.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@export var color_palette: ColorPaletteResource
---
+++ scripts/color_palette_resource.gd
@@ -0,0 +1,5 @@
+class_name ColorPaletteResource extends Resource
+
+@export var accent: Color = Color.RED
+@export var dark: Color = Color.BLACK
+@export var light: Color = Color.WHITE
