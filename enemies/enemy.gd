0a58513b58725f2f43cb7280b83c71dc8af2bd3c
Leo Feng <thcheetah777@gmail.com>
2024-02-11 08:52:45 -0600

    🧃 destroy enemies juicier

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -13,6 +13,9 @@ func get_hurt():
 		die()
 
 func die():
+	await Clock.hitstop(0.04)
+	await sprite.impact_expand(1.5, 0.05)
+	Globals.camera.shake(0.15, 3)
 	queue_free()
 
 func _on_area_entered(area: Area2D) -> void:
