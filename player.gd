00b573be9a5522ef3994107863b32771cda46041
Leo Feng <thcheetah777@gmail.com>
2024-02-11 08:29:06 -0600

    🧃 JUICYYY

--- player.gd
+++ player.gd
@@ -1,18 +1,20 @@
 class_name Player extends CharacterBody2D
 
-@export_category("Animation")
 @export var walk_tilt = 5.0
 @export var walk_tilt_speed = 1500.0
 @export var bat_rotation_offset = 90.0
 @export var bat_rotation_dynamics: DynamicsResource
+@export var impulse_damping = 500.0
 
 @onready var sprite: Sprite = $SpritePlus
 @onready var bat: Node2D = $Bat
-@onready var bat_sprite: Sprite2D = $Bat/BatSprite
+@onready var bat_sprite: Sprite = $Bat/BatSprite
 @onready var parry_area: Area2D = $ParryArea
 
 var bat_rotation = 0.0
 var bat_rotation_dynamics_solver: DynamicsSolver
+var impulse_velocity = Vector2.ZERO
+var mouse_angle: float
 
 func _enter_tree() -> void:
 	Globals.player = self
@@ -22,26 +24,28 @@ func _ready() -> void:
 	bat_rotation_dynamics_solver = Dynamics.create_dynamics(bat_rotation_dynamics)
 
 func _process(_delta: float) -> void:
-	var mouse_angle = get_angle_to(get_global_mouse_position()) + PI/2
+	mouse_angle = get_angle_to(get_global_mouse_position()) + PI/2
 	bat.rotation = mouse_angle + bat_rotation_dynamics_solver.update(deg_to_rad(bat_rotation))
 	parry_area.rotation = mouse_angle
+	bat_sprite.target_rotation_degrees = bat.rotation_degrees
 
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("left", "right", "up", "down")
-	velocity = direction * Stats.stats.movement_speed * delta
-
-	if direction:
-		sprite.target_rotation_degrees = sin(Clock.time * walk_tilt_speed * delta) * walk_tilt
-	else:
-		sprite.target_rotation_degrees = 0
+	var input = Input.get_vector("left", "right", "up", "down")
+	velocity = input * Stats.stats.movement_speed * delta + impulse_velocity
+	impulse_velocity = impulse_velocity.move_toward(Vector2.ZERO, impulse_damping * delta)
+	sprite.target_rotation_degrees = sin(Clock.time * walk_tilt_speed * delta) * walk_tilt if input else 0.0
 
 	if Input.is_action_just_pressed("lmb"):
 		swing_bat()
 
 	move_and_slide()
 
+func knockback(direction: Vector2, force: float):
+	impulse_velocity = direction.normalized() * force
+
 func swing_bat():
 	bat_rotation = -bat_rotation
+	knockback(position - get_global_mouse_position(), 100)
 
 	var bullets = parry_area.get_overlapping_areas().filter(func(area): return area is Bullet)
 	for bullet in bullets:
@@ -49,3 +53,4 @@ func swing_bat():
 		await Clock.wait(distance / 1000)
 		bullet.switch_to_player()
 		Clock.hitstop(0.04)
+		knockback(position - get_global_mouse_position(), 200)
