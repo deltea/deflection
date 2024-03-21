30f70c718cd2757ce1bd3d0200214d777925eabe
Leo Feng <thcheetah777@gmail.com>
2024-03-21 10:34:46 -0300

    ✅ button stuff

--- ui/button_2d.gd
+++ ui/button_2d.gd
@@ -1,9 +1,23 @@
 class_name Button2D extends Area2D
 
+@export var press_scale = 0.95
+@export var hover_scale = 1.1
 @export var sprite: Sprite
 
+var hovering = false
+var pressing = false
+
 func _on_mouse_entered() -> void:
-	if sprite: sprite.target_scale = Vector2.ONE * 1.1
+	hovering = true
+	if sprite: sprite.target_scale = hover_scale * Vector2.ONE
 
 func _on_mouse_exited() -> void:
-	if sprite: sprite.target_scale = Vector2.ONE * 1.0
+	hovering = false
+	if sprite: sprite.target_scale = 1.0 * Vector2.ONE
+
+func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		pressing = event.pressed
+		if sprite:
+			var target_scale = press_scale if pressing else hover_scale
+			sprite.target_scale = target_scale * Vector2.ONE
